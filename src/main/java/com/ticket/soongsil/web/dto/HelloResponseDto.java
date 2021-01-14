3234a9674192ec107f6233d1b697511cd7f64756
songhee27 <songhee27@ajpark.co.kr>
2021-01-14 19:12:01 +0900

    Initial commit

---
+++ src/main/java/com/ticket/soongsil/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.ticket.soongsil.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
